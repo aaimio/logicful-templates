7ed5a01aca45041cf3c8ceb7e56ab7ece73f7acc
Aaron Imming <aaim@protonmail.com>
2022-02-16 15:03:37 +0800

    fix: check for whitespace around element tags

--- src/components/__tests__/Magic.test.tsx
+++ src/components/__tests__/Magic.test.tsx
@@ -2,7 +2,7 @@ import LogicfulTemplates, { Magic } from '../..';
 
 describe('<Magic> "component"', () => {
   it('hoists its children when passing a "hoist" prop', () => {
-    const CustomElement1 = (
+    const createCustomElement = () => (
       <div className='some_class'>
         <Magic hoist>
           <span>Hello World</span>
@@ -10,8 +10,14 @@ describe('<Magic> "component"', () => {
       </div>
     );
 
-    const result1 = LogicfulTemplates.compileTemplate(CustomElement1);
+    const result1 = LogicfulTemplates.compileTemplate(createCustomElement);
     expect(result1).toBe('<div class="some_class"><span>Hello World</span></div>');
+
+    const result2 = LogicfulTemplates.compileTemplate(createCustomElement, { pretty: true });
+    expect(result2).toBe(`<div class="some_class">
+  <span>Hello World</span>
+</div>
+`);
   });
 
   it('compiles components later when passing "compileLast" props', () => {
@@ -91,35 +97,89 @@ describe('<Magic> "component"', () => {
   });
 
   it('compiles components later when passing "compileLast" props (mix of boolean + number)', () => {
-    const mockFn1 = jest.fn();
-    const mockFn2 = jest.fn();
+    const createCustomElement = () => {
+      const mockFn1 = jest.fn();
+      const mockFn2 = jest.fn();
 
-    const CustomElement1 = (
-      <div className='some_class'>
-        <Magic compileLater={1}>
-          {() => {
-            mockFn1();
-            expect(mockFn1).toHaveBeenCalledTimes(1);
-            expect(mockFn2).toHaveBeenCalledTimes(1);
-          }}
-          <span>Hello World</span>
-        </Magic>
-        <div className='some_other_class'>
-          <Magic compileLater>
+      return (
+        <div className='some_class'>
+          <Magic compileLater={1}>
             {() => {
-              mockFn2();
-              expect(mockFn1).toHaveBeenCalledTimes(0);
+              mockFn1();
+              expect(mockFn1).toHaveBeenCalledTimes(1);
               expect(mockFn2).toHaveBeenCalledTimes(1);
-              return <span>Goodbye Mars</span>;
             }}
+            <span>Hello World</span>
           </Magic>
+          <div className='some_other_class'>
+            <Magic compileLater>
+              {() => {
+                mockFn2();
+                expect(mockFn1).toHaveBeenCalledTimes(0);
+                expect(mockFn2).toHaveBeenCalledTimes(1);
+                return <span>Goodbye Mars</span>;
+              }}
+            </Magic>
+          </div>
         </div>
-      </div>
-    );
+      );
+    };
 
-    const result1 = LogicfulTemplates.compileTemplate(CustomElement1);
+    const result1 = LogicfulTemplates.compileTemplate(createCustomElement());
+
     expect(result1).toBe(
       '<div class="some_class"><span>Hello World</span><div class="some_other_class"><span>Goodbye Mars</span></div></div>'
+    );
+
+    const result2 = LogicfulTemplates.compileTemplate(createCustomElement(), { pretty: true });
+
+    expect(result2).toBe(`<div class="some_class">
+  <span>Hello World</span>
+  <div class="some_other_class">
+    <span>Goodbye Mars</span>
+  </div>
+</div>
+`);
+  });
+
+  it('handles deeply nested <Magic> components without issues', () => {
+    const DeeplyNestedMagic = () => (
+      <html>
+        <body>
+          <div className='deeply-nested-magic'>
+            <Magic hoist>
+              {() => (
+                <div className='level-1'>
+                  <Magic hoist>
+                    {() => (
+                      <div className='level-2'>
+                        <Magic hoist>
+                          <div className='level-3'>
+                            <Magic compileLater>
+                              {() => (
+                                <Magic hoist>
+                                  <Magic compileLater>
+                                    <div className='level-4'>Hello World</div>
+                                  </Magic>
+                                </Magic>
+                              )}
+                            </Magic>
+                          </div>
+                        </Magic>
+                      </div>
+                    )}
+                  </Magic>
+                </div>
+              )}
+            </Magic>
+          </div>
+        </body>
+      </html>
+    );
+
+    const result1 = LogicfulTemplates.compileTemplate(<DeeplyNestedMagic />);
+    expect(result1).toBe(
+      '<html><body><div class="deeply-nested-magic"><div class="level-1"><div class="level-2"><div class="level-3"><div class="level-4">Hello World</div></div></div></div></div></body></html>'
     );
   });
 });
